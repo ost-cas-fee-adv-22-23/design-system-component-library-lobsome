0376ffe0b5521748b942f31f90eb0e0e888831d4
Alexander Widmer <alexander.widmer@me.com>
2023-01-15 14:22:34 +0100

    feat(modal-container): new modal-container

--- src/components/Modal-Container.tsx
+++ src/components/Modal-Container.tsx
@@ -1,21 +1,54 @@
 import React, { MouseEvent, ReactNode } from 'react';
 import Cancel from './icons/Cancel';
+import { Button, ButtonColors } from './Button';
+import Checkmark from './icons/Checkmark';
 
 export interface ModalContainerProps {
     title: string;
-    onCancel: (e: MouseEvent<HTMLButtonElement>) => void;
+    cancelLabel?: string;
+    confirmLabel?: string;
+    onCancel?: (e: MouseEvent<HTMLButtonElement>) => void;
+    onSave?: (e: MouseEvent<HTMLButtonElement>) => void;
     children?: ReactNode;
 }
 
-export const ModalContainer = ({ title, onCancel, children, ...rest}: ModalContainerProps) => {
-    return <div className={['flex', 'flex-col', 'items-center', 'p-0', 'rounded-2xl'].join(' ')}>
-        <div className={['flex', 'flex-row', 'justify-between', 'items-center', 'gap-2', 'py-6', 'px-8'].join(' ')}>
-            <button onClick={onCancel}>
-                <Cancel />
-            </button>
+export const ModalContainer = ({ title, onCancel, onSave, children, cancelLabel = 'Cancel', confirmLabel = 'Save' }: ModalContainerProps) => {
+    return (
+        <div className={['flex', 'flex-col', 'items-center', 'p-0', 'rounded-2xl', 'w-[650px]', 'border-2', 'border-slate-200'].join(' ')}>
+            <div
+                className={[
+                    'bg-violet-600',
+                    'flex',
+                    'flex-row',
+                    'justify-between',
+                    'items-center',
+                    'gap-2',
+                    'py-6',
+                    'px-8',
+                    'rounded-t-2xl',
+                    'text-white',
+                    'w-full',
+                    'font-semibold',
+                    'text-[32px]',
+                    'leading-125'
+                ].join(' ')}
+            >
+                {title}
+                <button onClick={onCancel}>
+                    <Cancel />
+                </button>
+            </div>
+            <div className={['p-8'].join(' ')}>
+                <div>{children}</div>
+                <div className={['flex', 'items-center', 'gap-4', 'pt-8'].join(' ')}>
+                    <Button label={cancelLabel} onClick={onCancel} fullWidth={true}>
+                        <Cancel />
+                    </Button>
+                    <Button label={confirmLabel} color={ButtonColors.VIOLET} onClick={onSave} fullWidth={true}>
+                        <Checkmark />
+                    </Button>
+                </div>
+            </div>
         </div>
-        <div>{children}</div>
-        <div className={['flex', 'items-center', 'gap-4'].join(' ')}>
-        </div>
-    </div>;
+    );
 };
